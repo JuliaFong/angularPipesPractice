20c0e943fb407cf7d9737344038571a984f93140
Julia Fong <Juliamzfong@gmail.com>
2020-04-27 15:16:28 -0500

    Add a filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ShortenPipe } from './shorten.pipe';
 
 
 import { AppComponent } from './app.component';
-
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
